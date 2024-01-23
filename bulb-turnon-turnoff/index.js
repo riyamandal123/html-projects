110a2cc397c71bc30d9a4e4dbcb6e03ab5b2ead0
riyamandal123 <96898236+riyamandal123@users.noreply.github.com>
2024-01-23 20:51:17 +0530

    Add files via upload

---
+++ bulb-turnon-turnoff/index.js
@@ -0,0 +1,6 @@
+function turnOnLight() {
+    document.getElementById('myImage').src = 'bulb__1_-removebg-preview.png';
+}
+function turnOffLight()  {
+    document.getElementById('myImage').src = 'bulb-removebg-preview.png';
+}
